eeef6e70c86e86cd6c4f9bf571b86a3771bae6cd
gafelson <gafelson.soheil@gmail.com>
2025-02-26 16:32:45 +0330

    soheil 𖢥 stable version

--- app/bot/context.py
+++ app/bot/context.py
@@ -1,8 +1,8 @@
+# app.bot.context.py
 from telegram.ext import CallbackContext
-
 from app.database import Database
 
-
+# Database Context Property
 class DatabaseContext(CallbackContext):
     @property
     def db(self):
