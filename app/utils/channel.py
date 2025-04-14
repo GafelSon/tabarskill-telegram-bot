8970d53f749ba2b08726e6d9426a502388c8035d
gafelson <gafelson.soheil@gmail.com>
2025-04-14 21:52:40 +0330

    Add archive channel support and improve code formatting

--- app/utils/channel.py
+++ app/utils/channel.py
@@ -1,36 +1,52 @@
 # app.utils.channel.py
 import os
 from functools import wraps
+
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.ext import ContextTypes
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 
 # Define the channel username
-channel = os.getenv('CHANNEL_USERNAME', '@gafelson')
+channel = os.getenv("CHANNEL_USERNAME", "@gafelson")
+archive = os.getenv("ARCHIVE_USERNAME", "@tabarskill_archive")
+
 
 # Define the channel membership check function
-async def check_channel_membership(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def check_channel_membership(
+    update: Update, context: ContextTypes.DEFAULT_TYPE
+):
     try:
         user = update.effective_user
-        chat_member = await context.bot.get_chat_member(chat_id=channel, user_id=user.id)
-        return chat_member.status in ['member', 'administrator', 'creator']
+        chat_member = await context.bot.get_chat_member(
+            chat_id=channel, user_id=user.id
+        )
+        return chat_member.status in ["member", "administrator", "creator"]
     except Exception:
         return False
+
 
 # Define the channel membership required decorator
 def require_channel_membership(func):
     @wraps(func)
     async def wrapper(update: Update, context: ContextTypes.DEFAULT_TYPE):
         message = (
-            f">خطای عدم عضویت در کانال\n"
-            f"🌟 دانشجوی محترم، خوش آمدید\! 🌟\n\n"
-            f"⚠️ برای دسترسی به تمامی امکانات و خدمات ربات، لطفاً ابتدا در کانال رسمی دانشگاه عضو شوید\.\n\n"
-            f"🔥 __پس از عضویت، به راحتی می‌توانید از تمامی قابلیت‌ها و خدمات اختصاصی ربات استفاده کنید\!__\n\n"
-            f">تظرتون هستیم و از شما استقبال می‌کنیم\! 🚀\n"
+            ">خطای عدم عضویت در کانال\n"
+            "🌟 دانشجوی محترم، خوش آمدید\! 🌟\n\n"
+            "⚠️ برای دسترسی به تمامی امکانات و خدمات ربات، لطفاً ابتدا در کانال رسمی دانشگاه عضو شوید\.\n\n"
+            "🔥 __پس از عضویت، به راحتی می‌توانید از تمامی قابلیت‌ها و خدمات اختصاصی ربات استفاده کنید\!__\n\n"
+            ">تظرتون هستیم و از شما استقبال می‌کنیم\! 🚀\n"
         )
 
         keyboard = [
-            [InlineKeyboardButton("🎫 عضویت در کانال", url=f"https://t.me/{channel[1:]}")],
-            [InlineKeyboardButton("✅ عضو شدم", callback_data="check_membership")]
+            [
+                InlineKeyboardButton(
+                    "🎫 عضویت در کانال", url=f"https://t.me/{channel[1:]}"
+                )
+            ],
+            [
+                InlineKeyboardButton(
+                    "✅ عضو شدم", callback_data="check_membership"
+                )
+            ],
         ]
         reply_markup = InlineKeyboardMarkup(keyboard)
 
@@ -38,23 +54,26 @@ def require_channel_membership(func):
             await update.message.reply_text(
                 text=message,
                 reply_markup=reply_markup,
-                parse_mode='MarkdownV2',
-                disable_web_page_preview=True
+                parse_mode="MarkdownV2",
+                disable_web_page_preview=True,
             )
             return
         return await func(update, context)
+
     return wrapper
 
+
 # Define the channel membership check callback
-async def check_membership_callback(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def check_membership_callback(
+    update: Update, context: ContextTypes.DEFAULT_TYPE
+):
     query = update.callback_query
     await query.answer()
-    
+
     if await check_channel_membership(update, context):
         await query.message.edit_text("🎟️ عضویت شما با موفقیت تایید شد!\n")
     else:
         await query.answer(
-            "🪃 هنوز عضو کانال نشده‌اید!\n"
-            "لطفاً ابتدا در کانال عضو شوید.",
-            show_alert=True
+            "🪃 هنوز عضو کانال نشده‌اید!\n" "لطفاً ابتدا در کانال عضو شوید.",
+            show_alert=True,
         )
