de287bcbad36ae0e76315d1fb2901c83b526c3f4
gafelson <gafelson.soheil@gmail.com>
2025-04-14 21:54:00 +0330

    Add Markdown escaping to echo handler

--- app/bot/handlers/echo.py
+++ app/bot/handlers/echo.py
@@ -1,17 +1,49 @@
 # app.bot.handlers.echo.py
 # THIS IS TEST HANDLER FOR TESTING PURPOSES
-from telegram.ext import ContextTypes
-from telegram import Update
-
 import logging
 
+from telegram import Update
+from telegram.ext import ContextTypes
+
 logger = logging.getLogger(__name__)
+
+
+def escape_markdown(text):
+    special_chars = [
+        "_",
+        "*",
+        "[",
+        "]",
+        "(",
+        ")",
+        "~",
+        "`",
+        ">",
+        "#",
+        "+",
+        "-",
+        "=",
+        "|",
+        "{",
+        "}",
+        ".",
+        "!",
+    ]
+    escaped_text = str(text)
+    for char in special_chars:
+        escaped_text = escaped_text.replace(char, f"\\{char}")
+    return escaped_text
+
 
 async def echo_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
     """Echo the user message."""
     if update.message and update.message.text:
-        await update.message.reply_text(
-            text=f"🤖 ربات: گفتی {update.message.text}؟ منم گفتم! حالا دو برابر مهم شد! 📢📢😆"
+        message_text = escape_markdown(update.message.text)
+        response_text = escape_markdown(
+            f"🤖 ربات: گفتی {message_text}؟ منم گفتم! حالا دو برابر مهم شد! 📢📢😆"
         )
+        await update.message.reply_text(text=response_text, parse_mode="MarkdownV2")
     else:
-        logger.warning(f"SYSTEM: Received update without message text: {update}")
+        logger.warning(
+            f"SYSTEM: Received update without message text: {update}"
+        )
