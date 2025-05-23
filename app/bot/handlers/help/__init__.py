37cfb8e51bb7145e6d8420cb04d10a8653eb0ac0
gafelson <gafelson.soheil@gmail.com>
2025-05-23 11:27:16 +0330

    refactor: remove help handler and modularize logic

---
+++ app/bot/handlers/help/__init__.py
@@ -0,0 +1,18 @@
+from .handler import handler, gotit
+from .learn import learn
+from .why import why
+from .call import call
+from .support import support
+from .feedback import feedback
+
+__all__ = [
+    # main function
+    "handler",
+    "gotit",
+    # other finction
+    "learn",
+    "why",
+    "call",
+    "support",
+    "feedback",
+]
---
+++ app/bot/handlers/help/handler.py
@@ -0,0 +1,57 @@
+# help handler finction
+
+# main lib
+
+# dependencies lib
+from telegram.ext import ContextTypes
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update, Message
+
+# local lib
+from app.utils.flags import has_flag
+from app.core.decor import message_object
+
+
+@message_object
+async def handler(
+    update: Update, context: ContextTypes.DEFAULT_TYPE, message: Message = None
+) -> None:
+    query = update.callback_query
+
+    onboarding = (
+        ">دستورالعمل\n\n\n"
+        "✨ *دستورالعمل استفاده از ربات* ✨\n\n"
+        "به پنل راهنمای ربات دستیار دانشگاهی دانشگاه ملی مهارت مازندران خوش آمدید\. در اینجا می‌توانید به راحتی به امکانات دانشگاه و خدمات مختلف دسترسی داشته باشید\.\n\n"
+        "*🌟 امیدواریم بتوانید بیشترین استفاده را از این ربات داشته باشید و در امور دانشگاهی به شما کمک کند\!*\n\n"
+        ">اگر سوالی داشتید یا نیاز به کمک بیشتری داشتید، می‌توانید به راحتی از طریق ادمین راهنمایی بگیرید\."
+    )
+
+    keyboard = [
+        [InlineKeyboardButton("🕹️ آموزش ربات دستیار", callback_data="learn")],
+        [InlineKeyboardButton("💻 چرا این ربات فعالیت میکند؟", callback_data="why")],
+        [InlineKeyboardButton("👩‍💼 تماس با پشتیبانی ربات", callback_data="call")],
+        [InlineKeyboardButton("👨‍💻 تماس با پشتیبانی فنی", callback_data="support")],
+        [
+            InlineKeyboardButton(
+                "📝 فرم ارسال نظرات و پیشنهادات", callback_data="feedback"
+            )
+        ],
+    ]
+    keyboard_layout = InlineKeyboardMarkup(keyboard)
+
+    await message.reply_photo(
+        photo="AgACAgQAAyEGAASLt5ydAAMmZ_yo0BP-GMN8Vjv7pn9FojWPr4IAAnDGMRstPuFT2ygGVy3kLJ8BAAMCAANtAAM2BA",
+        caption=onboarding,
+        reply_markup=keyboard_layout,
+        parse_mode="MarkdownV2",
+    )
+
+
+async def gotit(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    query = update.callback_query
+
+    if not query:
+        logger.error("SYSTEM:: GotItHandler:: No callback query found in update")
+        return
+
+    await query.message.delete()
+    await query.answer()
