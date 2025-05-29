23db8718f6265c267738157098b9874fcc2bbcda
gafelson <gafelson.soheil@gmail.com>
2025-05-29 14:17:27 +0330

    add edit bio callback

--- app/bot/handlers/bio/__init__.py
+++ app/bot/handlers/bio/__init__.py
@@ -1,7 +1,10 @@
 from .handler import handler
+from .edit_profile import start_profile_edit
 
 
 __all__ = [
-    # main function
-    "handler"
+    # main functions
+    "handler",
+    # other functions
+    "start_profile_edit",
 ]
