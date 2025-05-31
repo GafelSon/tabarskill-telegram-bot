8e97b1477931314502c5f457cb9c6d29161e3a1b
gafelson <gafelson.soheil@gmail.com>
2025-05-31 14:44:54 +0330

    Export new event callbacks and module in reminder init

--- app/bot/handlers/reminder/__init__.py
+++ app/bot/handlers/reminder/__init__.py
@@ -1,9 +1,16 @@
-from .handler import handler, _back
+from .handler import handler, module, _back
 from .help import events_help
+
+from .callbacks.event_new import new_event_callback
+from .callbacks.event_type import new_personal_event, new_university_event
 
 all = [
     # main functions
     "handler",
+    "module",
     # other functions
     "events_help",
+    "new_event_callback",
+    "new_personal_event",
+    "new_university_event",
 ]
